9808464cffdb00a76b1b8924beabbceea77274dc
Olya <zaychenok37@gmail.com>
2021-02-26 12:04:05 +0200

    Add unfinished buttons

--- src/Components/Calendar/CalendarBody/index.jsx
+++ src/Components/Calendar/CalendarBody/index.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
-import { format, getYear, getMonth } from 'date-fns';
+import { format, getYear, getMonth, addMonths } from 'date-fns';
 import Month from '../Month';
 import styles from './CalendarBody.module.scss';
+
 
 const WeekDays = () => {
   return (
@@ -20,17 +21,24 @@ const WeekDays = () => {
 const CalendarBody = props => {
 
   const { currentDay } = props;
+
   const nowYear = getYear(currentDay);
   const nowMonth = getMonth(currentDay) + 1;
+
+  const monthPrev = addMonths(nowMonth, -1);
+  console.log(monthPrev);
+
+  const monthNext = addMonths(nowMonth, +1);
+  console.log(monthPrev);
 
   return (
     <div className={styles.monthWrapper}>
       <table>
 
         <caption className={styles.monthAndYear}>
-          <button onClick={currentDay.handlerMonthPrev}>{'<'}</button>
+          <button onClick={monthPrev}>{'<'}</button>
           {format(currentDay, 'MMMM y')}
-          <button onClick={currentDay.handlerMonthNext}>{'>'}</button>
+          <button onClick={monthNext}>{'>'}</button>
         </caption>
 
         <thead className={styles.daysOfWeek}>
--- src/Components/Calendar/index.jsx
+++ src/Components/Calendar/index.jsx
@@ -1,25 +1,17 @@
 import React, { Component } from 'react';
 import CurrentDay from './CurrentDay';
 import CalendarBody from './CalendarBody';
-import { addMonths } from 'date-fns';
 import styles from './Calendar.module.scss';
 
 
 export default class Calendar extends Component {
+  
   constructor (props) {
     super(props);
     this.state = {
       currentDay: new Date(),
     };
   }
- 
-  handlerMonthPrev = () => {
-    this.setState({ currentDay: addMonths(this.state.currentDay, -1) });
-  };
-
-  handlerMonthNext = () => {
-    this.setState({ currentDay: addMonths(this.state.currentDay, +1) });
-  };
 
   render () {
     return (
